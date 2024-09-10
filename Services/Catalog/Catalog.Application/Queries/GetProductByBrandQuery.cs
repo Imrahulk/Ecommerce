3fdc52ac3bdfaf2ac1bc84a275f009800e388873
rk@delphime.com <Minnukutty1!>
2024-09-11 00:48:49 +0530

    Implemented ProductByBrand

---
+++ Services/Catalog/Catalog.Application/Queries/GetProductByBrandQuery.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+using System.Reflection.Metadata;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductByBrandQuery : IRequest<IList<ProductResponse>>
+    {
+        public string BrandName { get; set; }
+        public GetProductByBrandQuery(string brandName)
+        {
+            BrandName = brandName;
+        }
+    }
+}
