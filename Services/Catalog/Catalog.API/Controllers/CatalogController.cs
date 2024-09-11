1e80130a74ea56c3d744df7839016eeca97a7f0c
rk@delphime.com <ping2rahulk@gmail.com>
2024-09-12 03:28:56 +0530

    Implemented Catalog controller methods

---
+++ Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Catalog.API.Controllers
+{
+    public class CatalogController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public CatalogController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("[action]/{id}", Name = "GetProductById")]
+        [ProducesResponseType(typeof(ProductResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<ProductResponse>> GetProductId(string id)
+        {
+            var query = new GetProductByIdQuery(id);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+
+        }
+    }
+}
