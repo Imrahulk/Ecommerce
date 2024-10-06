dd875199d5f1bf7c083db39012d599738b1a4fd1
Rahul Delphi <ping2rahulk@gmail.com>
2024-10-07 01:18:52 +0530

    Implemented search and sort functionality using the Specification pattern

--- Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
+++ Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -13,7 +13,7 @@ namespace Catalog.Infrastructure.Repositories
         public ProductRepository(ICatalogContext context)
         {
             _context = context;
-        } 
+        }
         async Task<Product> IProductRepository.GetProduct(string id)
         {
             return await _context.Products
@@ -23,13 +23,58 @@ namespace Catalog.Infrastructure.Repositories
 
         async Task<Pagination<Product>> IProductRepository.GetProducts(CatalogSpecParams catalogSpecParams)
         {
-            var filter = Builders<Product>.Filter.Empty;
-            return await _context
-                .Products
-                .Find(p => true)
-                .ToListAsync();
+            var builder = Builders<Product>.Filter;
+            var filter = builder.Empty;
+            if (!string.IsNullOrEmpty(catalogSpecParams.Search))
+            {
+                filter = filter & builder.Where(p => p.Name.ToLower().Contains(catalogSpecParams.Search.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(catalogSpecParams.BrandId))
+            {
+                var brandFilter = builder.Eq(p => p.Brands.Id, catalogSpecParams.BrandId);
+                filter &= brandFilter;
+            }
+            if (!string.IsNullOrEmpty(catalogSpecParams.TypeId))
+            {
+                var typeFilter = builder.Eq(p => p.Types.Id, catalogSpecParams.TypeId);
+                filter &= typeFilter;
+            }
+            var totalItems = await _context.Products.CountDocumentsAsync(filter);
+            var data = await DataFilter(catalogSpecParams, filter);
+            return new Pagination<Product>(
+                catalogSpecParams.PageIndex,
+                catalogSpecParams.PageSize,
+                (int)totalItems,
+                data);
         }
 
+        private async Task<IReadOnlyList<Product>> DataFilter(CatalogSpecParams catalogSpecParams, FilterDefinition<Product> filter)
+        {
+            var sortDefn = Builders<Product>.Sort.Ascending("Name");
+            if (!string.IsNullOrEmpty(catalogSpecParams.Sort))
+            {
+                switch (catalogSpecParams.Sort)
+                {
+                    case "priceAsc":
+                        sortDefn = Builders<Product>.Sort.Ascending(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        sortDefn = Builders<Product>.Sort.Descending(p => p.Price);
+                        break;
+                    default:
+                        sortDefn = Builders<Product>.Sort.Ascending(p => p.Price);
+                        break;
+                }
+            }
+            return await _context
+                .Products
+                .Find(filter)
+                .Sort(sortDefn)
+                .Skip((catalogSpecParams.PageIndex - 1) * catalogSpecParams.PageSize)
+                .Limit(catalogSpecParams.PageSize)
+                .ToListAsync();
+
+        }
         async Task<IEnumerable<Product>> IProductRepository.GetProductsByBrand(string brandName)
         {
             return await _context
@@ -46,7 +91,7 @@ namespace Catalog.Infrastructure.Repositories
                 .ToListAsync();
         }
 
-       async Task<Product> IProductRepository.CreateProduct(Product product)
+        async Task<Product> IProductRepository.CreateProduct(Product product)
         {
             await _context.Products.InsertOneAsync(product);
             return product;
@@ -54,16 +99,16 @@ namespace Catalog.Infrastructure.Repositories
 
         async Task<bool> IProductRepository.DeleteProduct(string id)
         {
-           var deletedProduct = await _context.Products
-                 .DeleteOneAsync(p => p.Id == id);
+            var deletedProduct = await _context.Products
+                  .DeleteOneAsync(p => p.Id == id);
             return deletedProduct.IsAcknowledged && deletedProduct.DeletedCount > 0;
         }
         async Task<bool> IProductRepository.UpdateProduct(Product product)
         {
             var updatedProduct = await _context
                 .Products
-                .ReplaceOneAsync(p => p.Id == product.Id,product);
-                
+                .ReplaceOneAsync(p => p.Id == product.Id, product);
+
             return updatedProduct.IsAcknowledged && updatedProduct.ModifiedCount > 0;
         }
 
@@ -83,6 +128,6 @@ namespace Catalog.Infrastructure.Repositories
                 .ToListAsync();
         }
 
-       
+
     }
 }
