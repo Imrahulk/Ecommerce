73ff8594c483e1b8943a46c5ab9a385583e3c56f
Rahul Delphi <ping2rahulk@gmail.com>
2024-10-21 23:25:41 +0530

    Added CQRS Commands for Creating shopping cart.

---
+++ Services/Basket/Basket.Application/Commands/CreateShoppingCartCommand.cs
@@ -0,0 +1,18 @@
+﻿using Basket.Application.Responses;
+using Basket.Core.Entities;
+using MediatR;
+
+namespace Basket.Application.Commands
+{
+    public class CreateShoppingCartCommand : IRequest<ShoppingCartResponse>
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+        public CreateShoppingCartCommand(string userName, List<ShoppingCartItem> item)
+        {
+            UserName = userName;
+            Items = item;
+
+        }
+    }
+}
