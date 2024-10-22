de62e3f71edbeebeba09cf02f0c7a1720549984e
Rahul Delphi <ping2rahulk@gmail.com>
2024-10-23 01:06:14 +0530

    Added Discount Repository

---
+++ Services/Discount/Discount.Core/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Core.Entities;
+
+namespace Discount.Core.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(string productName);
+        Task<bool> UpdateDiscount(string productName);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
---
+++ Services/Discount/Discount.Infrastructure/Repositories/DiscountRepository.cs
@@ -0,0 +1,32 @@
+﻿using Discount.Core.Entities;
+using Discount.Core.Repositories;
+
+namespace Discount.Infrastructure.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        public DiscountRepository()
+        {
+            
+        }
+        public Task<bool> CreateDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Coupon> GetDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
