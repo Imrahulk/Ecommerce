e3718b05575691d7778bcb81dd396d16de343f10
Rahul Delphi <ping2rahulk@gmail.com>
2025-01-04 04:22:15 +0530

    Implemented Discount Project and grpc calls

---
+++ Services/Discount/Discount.Application/Handlers/UpdateDiscountCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Discount.Application.Commands;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using Discount.Grpc.Protos;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    public class UpdateDiscountCommandHandler : IRequestHandler<UpdateDiscountCommand, CouponModel>
+    {
+        private readonly IDiscountRepository _discountRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateDiscountCommandHandler(IDiscountRepository discountRepository, IMapper mapper)
+        {
+            _discountRepository = discountRepository;
+            _mapper = mapper;
+        }
+        public Task<CouponModel> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var coupon = _mapper.Map<Coupon>(request);
+        }
+    }
+}
